2d1fa94dcbb8db8386573aa3c6d67d75872d3109
Anshuman <107368860+anshuman008@users.noreply.github.com>
2024-08-16 21:03:17 +0530

    intial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+'use client'
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 
 export default function Home() {
+
+  const session = useSession();
   return (
-    <div>hello bhaii</div>
+    <div className="h-screen bg-white flex justify-center items-center">
+
+      <div className="flex flex-col justify-center items-center gap-y-10">
+
+        <div className="flex flex-col justify-center items-center gap-y-5">
+          <span className="text-6xl font-bold text-white">The Indian Cryptocurrency <span className="text-orange-500">Revolution</span></span>
+          <span className="text-3xl font-bold text-white">Create a frictionless wallet from India with just a Google Account.</span>
+          <span className="text-3xl font-bold text-white">Convert your INR into Cryptocurrency</span>
+        </div>
+        {/* <span className="p-3 bg-blue-:400 px-10 rounded-lg ">Dashboard</span> */}
+
+
+        <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+          {session?.data?.user ? 'dashboard' : 'Sign In'}
+        </button>
+
+      </div>
+
+
+    </div>
   );
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { signIn, signOut, useSession } from "next-auth/react"
+
+const NavBar = () => {
+
+
+  const session = useSession();
+
+  
+  return (
+        <div className="flex w-full justify-between px-10 items-center h-12 p-9 bg-[#1c1c1c]">
+            <div className="text-2xl font-bold text-white">Dcex</div>
+
+            <button type="button" className="text-white bg-black hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-lg px-8 py-1  me-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 " onClick={() => {session?.data?.user? signOut():signIn()}}>{session?.data?.user ?'Sign Out' :'Sign In'}</button>
+            
+        </div>
+  )
+}
+
+export default NavBar
